d46ba9fff822b2e7acf4c76e61e54d2f90ef84ad
joaovstt <joaovsilvateixeira@gmail.com>
2024-05-29 00:34:48 -0300

    inclusão da contato module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { NossaVisaoComponent } from './nossa-visao/nossa-visao.component';
+import { ContatoModule } from './contato/contato.module';
+
+
 
 export const routes: Routes = [];
